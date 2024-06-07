61041be7983e19a8a8a72cf5ef94b96b7ddc8479
Theroiz <164636377+Theroiz@users.noreply.github.com>
2024-06-06 21:36:59 -0300

    Add files via upload
    Exercicio compras

---
+++ src/ShoppingCart01.java
@@ -0,0 +1,17 @@
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+        // Declare and initialize String variables.  Do not initialize message yet.
+        String custName = "heitor ";
+        String itemDesc = "quer ";
+        String message = "comprar roupa";
+        
+        System.out.println(custName+itemDesc+message);
+        
+        
+        // Assign the message variable 
+        
+        
+        // Print and run the code
+        
+    }
+}
